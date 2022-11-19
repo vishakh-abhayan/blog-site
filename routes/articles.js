492486a4924777a7e9a2b1b83cee8b8dc9096399
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-11-19 20:20:07 +0200

    data base connected

--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const { models } = require("mongoose");
+const mongoose = require("mongoose");
 const router = express.Router();
+
+mongoose.connect("mongodb://localhost/blog ");
 
 router.get("/new", (req, res) => {
   res.render("articles/new");
